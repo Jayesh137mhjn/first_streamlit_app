815896ffd9eb7ae5f5ccc0e48793888b8c9fcc94
Jayesh137mhjn <124644778+Jayesh137mhjn@users.noreply.github.com>
2023-03-17 12:05:14 +0530

    Update streamlit_app.py
    menu added in the header section

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.title('Boiled Eggs')
+streamlit.title('Veg fried rice')
+streamlit.title('Biryani')
