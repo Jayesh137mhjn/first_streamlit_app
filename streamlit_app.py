f26c646149b5866439a1f05469d40b97da46aee5
Jayesh137mhjn <124644778+Jayesh137mhjn@users.noreply.github.com>
2023-03-17 12:09:05 +0530

    Update streamlit_app.py
    changes done in app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
-streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Boiled Eggs')
-streamlit.text('Veg fried rice')
-streamlit.text('Biryani')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
